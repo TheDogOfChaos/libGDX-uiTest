9ed1e6a9e80ea6386649e20167219c4ba6b6b16c
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-02-01 16:22:56 +0000

    fuck

--- desktop/src/io/thedogofchaos/drop/DesktopLauncher.java
+++ desktop/src/io/thedogofchaos/drop/DesktopLauncher.java
@@ -1,15 +1,17 @@
-package io.thedogofchaos.uitest;
+package io.thedogofchaos.drop;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.thedogofchaos.uitest.uiTest;
+import io.thedogofchaos.drop.Drop;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
 		config.setForegroundFPS(60);
-		config.setTitle("myUITest");
-		new Lwjgl3Application(new uiTest(), config);
+		new Lwjgl3Application(new Drop(), config);
 	}
 }
