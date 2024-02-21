80b3836546d7e3a7959e2f51d26218cd6089cae8
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-02-21 10:56:31 +0000

    fuck this

---
+++ core/src/io/thedogofchaos/uitest/uiTest.java
@@ -0,0 +1,54 @@
+package io.thedogofchaos.uitest;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class uiTest extends ApplicationAdapter {
+	private Skin skin;
+	private Stage stage;
+
+	@Override
+	public void create () {
+		skin = new Skin(Gdx.files.internal("metalui/metal-ui.json"));
+
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
+
+		Table root = new Table();
+		root.setFillParent(true);
+		stage.addActor(root);
+
+		root.pad(25);
+		TextButton textButton = new TextButton("Fire!", skin);
+		root.add(textButton).expand().bottom().left();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(.9f, .9f, .9f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act();
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void dispose () {
+		skin.dispose();
+		stage.dispose();
+	}
+}
---
+++ desktop/src/io/thedogofchaos/uitest/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package io.thedogofchaos.uitest;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import io.thedogofchaos.uitest.uiTest;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("uiTest");
+		new Lwjgl3Application(new uiTest(), config);
+	}
+}
