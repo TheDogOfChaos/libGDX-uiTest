f0cd0dfffea5088f8a72cfa48f6a61f25fc5d66a
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-02-29 15:11:27 +0000

    Added custom Skin
    done via https://github.com/raeleus/skin-composer

--- core/src/io/thedogofchaos/uitest/Vars.java
+++ core/src/io/thedogofchaos/uitest/Vars.java
@@ -5,6 +5,6 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class Vars {
-    public static Skin gameSkin = new Skin(Gdx.files.internal("metalui/metal-ui.json"));
+    public static Skin gameSkin = new Skin(Gdx.files.internal("mindusStyle/mindusStyle.json"));
     public static String currentStage;
 }
